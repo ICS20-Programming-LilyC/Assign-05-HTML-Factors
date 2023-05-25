9b2e902a716f38fce9338ab8fd5b2831782f2ec8
Lilyyyc <124380100+Lilyyyc@users.noreply.github.com>
2023-05-25 15:49:55 +0000

    code does not work but updated to flowchart

--- js/script.js
+++ js/script.js
@@ -4,21 +4,34 @@
 // Created on: 05/17/2023
 // This file contains the JS functions for index.html
 
-"use strict"
+"use strict";
 
 function factorsSent() {
   let counter = 1;
   let numberString = "";
   let message = "";
-
+  
+// Initalizing variable and constant
+  const radio = document.getElementById("reverse-yes");
   let number = parseInt(document.getElementById("number").value);
 
   if (isNaN(number)) {
-    message = "Please enter valid numbers.";
-  } 
+    message = "Please enter a valid number.";
+  } else if {
+    (radio.checked) {
+      counter = number;
+    } else {
+      counter = 1;
+    }
 
-  // Else...
-  else {
+    if (number < 0) {
+      counter = -1;
+      number = Math.abs(number);
+      message = "Here is a list of all the factors for the negative number you entered:<br><br>";
+    } else {
+      message = "Here is a list of all the factors for the number you entered:<br><br>";
+    }
+
     while (counter <= number) {
       if (number % counter === 0) {
         numberString += counter + "<br>";
@@ -26,7 +39,13 @@ function factorsSent() {
       counter++;
     }
 
-    message = "Here is a list of all the factors for the number you entered:<br><br>" + numberString;
+    if (number % 2 === 0) {
+      message += "The number you entered is even.<br>";
+    } else {
+      message += "The number you entered is odd.<br>";
+    }
+
+    message += numberString;
   }
 
   document.getElementById("results").innerHTML = message;
