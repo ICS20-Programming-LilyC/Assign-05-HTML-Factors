3be109c8ac392c69f579226247fd0cfed03ff84f
Lilyyyc <124380100+Lilyyyc@users.noreply.github.com>
2023-05-27 19:03:59 +0000

    Got integers to display in negatives

--- js/script.js
+++ js/script.js
@@ -4,55 +4,53 @@
 // Created on: 05/17/2023
 // This file contains the JS functions for index.html
 
-"use strict";
+"use strict"
 
 function factorsSent() {
   let counter = 1;
   let numberString = "";
   let message = "";
-  
-// Initalizing variable and constant
-  const radio = document.getElementById("reverse-yes");
+
+  // Initializing variable and constant
+  const REVERSE_YES = document.getElementById("reverse-yes");
   let number = parseInt(document.getElementById("number").value);
 
   if (isNaN(number)) {
     message = "Please enter a valid number.";
   } 
+  
+  else if (REVERSE_YES.checked) {
+    counter = -number;
     
-  else if (radio.checked) {
-    counter = number;
   } 
-    
+  
   else {
-    counter = 1;
+    counter = -number;
   }
 
   if (number < 0) {
-      counter = -1;
-      number = Math.abs(number);
-    } 
-    
+    counter = -1;
+    number = Math.abs(number);
+  } 
+  
   else {
     message = "Here is a list of all the factors for your integer:<br><br>";
   }
 
   while (counter <= number) {
     if (number % counter == 0) {
-        numberString += counter + "<br>";
-      }
-      counter++;
+      numberString += counter + "<br>";
     }
+    counter++;
+  }
 
   if (number % 2 == 0) {
     message += "The integer you entered is even.<br>";
-  } 
-    
-  else {
+  } else {
     message += "The integer you entered is odd.<br>";
   }
 
-    message += numberString;
-  }
+  message += numberString;
 
   document.getElementById("results").innerHTML = message;
 }
