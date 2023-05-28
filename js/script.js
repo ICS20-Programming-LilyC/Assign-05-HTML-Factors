bd52d177044a9eb03333351fb3db35efaf719cee
Lilyyyc <124380100+Lilyyyc@users.noreply.github.com>
2023-05-28 00:35:05 +0000

    Got negative input working, negative factors to display, 0 input error and if nothing is inputted error. (Need to get it to work for reversed option.

--- js/script.js
+++ js/script.js
@@ -4,53 +4,67 @@
 // Created on: 05/17/2023
 // This file contains the JS functions for index.html
 
-"use strict"
+"use strict";
 
 function factorsSent() {
   let counter = 1;
   let numberString = "";
   let message = "";
 
-  // Initializing variable and constant
   const REVERSE_YES = document.getElementById("reverse-yes");
   let number = parseInt(document.getElementById("number").value);
+  let absNumber = Math.abs(number);
 
   if (isNaN(number)) {
     message = "Please enter a valid number.";
   } 
   
-  else if (REVERSE_YES.checked) {
-    counter = -number;
+  else if (number == 0) {
+    message = "Zero has no factors.";
+  } 
     
-  } 
+  else if (REVERSE_YES.checked == true) {
+    counter = -number;
+    message = "Here is a list of all the factors for your number:<br><br>";
+
+    for (counter = -absNumber; counter <= absNumber; counter++) {
+      if (absNumber % counter == 0) {
+        numberString += counter + "<br>";
+      }
+    }
+
+    if (number % 2 == 0) {
+      message += "The number you entered is even.<br>";
+    } 
+    
+    else {
+      message += "The number you entered is odd.<br>";
+    }
+
+    message += numberString;
+  }
   
   else {
     counter = -number;
-  }
+    message = "Here is a list of all the factors for your number:<br><br>";
 
-  if (number < 0) {
-    counter = -1;
-    number = Math.abs(number);
-  } 
-  
-  else {
-    message = "Here is a list of all the factors for your integer:<br><br>";
-  }
-
-  while (counter <= number) {
-    if (number % counter == 0) {
-      numberString += counter + "<br>";
+     for (counter = -absNumber; counter <= absNumber; counter++) {
+      if (number % counter == 0) {
+        numberString += counter + "<br>";
+      }
     }
-    counter++;
-  }
 
-  if (number % 2 == 0) {
-    message += "The integer you entered is even.<br>";
-  } else {
-    message += "The integer you entered is odd.<br>";
-  }
 
-  message += numberString;
+    if (number % 2 == 0) {
+      message += "The number you entered is even.<br>";
+    } 
+    
+    else {
+      message += "The number you entered is odd.<br>";
+    }
+
+    message += numberString;
+  }
 
   document.getElementById("results").innerHTML = message;
 }
